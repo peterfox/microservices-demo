26a6fffa5b5f02dcf4326141422c096a68605527
Peter Fox <slyfoxy@gmail.com>
2015-07-23 01:04:21 +0100

    Part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,27 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-aws-lambda');
 
   grunt.initConfig({
-      lambda_invoke: {
-          default: {
-              options: {
-              }
-          }
-      },
+    lambda_invoke: {
+      default: {
+        options: {
+        }
+      }
+    },
+    lambda_package: {
+      default: {
+        options: {
+        }
+      }
+    },
+    lambda_deploy: {
+      default: {
+        arn: 'arn:aws:lambda:eu-west-1:367845992893:function:microServicesDemo',
+        options: {
+          region: 'eu-west-1'
+        }
+      }
+    },
   });
+
+  grunt.registerTask('deploy', ['lambda_package', 'lambda_deploy']);
 };
